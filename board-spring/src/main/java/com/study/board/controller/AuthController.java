c618204751bc554c4c8966356b61c122b08d9bfb
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-03-01 15:31:51 +0900

    cors 설정

--- board-spring/src/main/java/com/study/board/controller/AuthController.java
+++ board-spring/src/main/java/com/study/board/controller/AuthController.java
@@ -2,17 +2,14 @@ package com.study.board.controller;
 
 import com.study.board.dto.ResponseDto;
 import com.study.board.dto.SignUpDto;
-import com.study.board.dto.SignUpResponseDto;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(originPatterns = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
     @PostMapping("/signUp")
-    public ResponseDto<SignUpResponseDto> signUp(@RequestBody SignUpDto requestBody) {
-        System.out.println(requestBody.toString());
+    public ResponseDto<?> signUp(@RequestBody SignUpDto requestBody) {
         return null;
     }
 }
