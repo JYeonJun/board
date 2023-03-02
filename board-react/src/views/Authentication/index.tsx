28229343d4ef09dc5e2d141ec30a4241455ada0d
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-03-02 13:40:14 +0900

    Front end 쿠키 및 스토어 사용

--- board-react/src/views/Authentication/index.tsx
+++ board-react/src/views/Authentication/index.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 
 export default function Authentication() {
-  return <SignUp />;
+  return (
+    <>
+      <SignUp />
+      <SignIn />
+    </>
+  );
 }
